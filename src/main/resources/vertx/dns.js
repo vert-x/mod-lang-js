a5b8c42fc20d6d26cbb62fcfd7b5f36284d49ec3
Lance Ball <lball@redhat.com>
2013-08-19 19:34:07 -0400

    Initial stab at async dns integration

---
+++ src/main/resources/vertx/dns.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+if (typeof __vertxload === 'string') {
+  throw "Use require() to load Vert.x API modules"
+}
+
+var console = require('vertx/console');
+
+/**
+ * @exports vertx/dns
+ */
+var dns = {
+  /**
+   * Creates and returns a DNS client object
+   *
+   * @param {Array|string} [servers] The DNS server address(es). If not supplied, defaults
+   *        to Google DNS servers 8.8.8.8 and 8.8.4.4
+   *        TODO: Figure out proper default behavior
+   * @returns {DnsClient} A DnsClient object 
+   */
+  createDnsClient: function(servers) {
+    if (typeof servers == 'undefined') {
+      servers = '8.8.8.8';
+    }
+    return new DnsClient(servers);
+  },
+};
+
+var DnsClient = function(servers) {
+  var that = this;
+
+  var wrapIpAddresses = function(servers) {
+    if (typeof servers === 'string') {
+      // TODO: This is non-ideal
+      inetAddress = java.net.InetAddress.getByName(servers);
+      return [new java.net.InetSocketAddress(inetAddress, 53)];
+    } 
+  }
+
+  console.log("VERTX JAVA FUNC: " + __jvertx.createDnsClient);
+
+  var __jClient = __jvertx.createDnsClient(wrapIpAddresses(servers));
+
+  this.lookup = function() {
+  }
+
+}
+
+module.exports = dns;
+
